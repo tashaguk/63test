4798f65a211be2879e4a46e4c5aa123bd4d1a89c
tashaguk <154615573+tashaguk@users.noreply.github.com>
2024-02-09 21:18:26 +0400

    Теперь окошек два!

--- HelloJava.java
+++ HelloJava.java
@@ -5,15 +5,14 @@ public class HelloJava {
     public static void main( String[] args ) {
 
     JFrame frame = new JFrame( "Hello, Java!" );
-
     JLabel label = new JLabel( "Hello, Java!", JLabel.CENTER );
-
+    JFrame frame1 = new JFrame( "Hi" );
+    JLabel label1 = new JLabel( "12345", JLabel.CENTER );
     frame.add( label );
-
-    frame.setSize( 300, 300 );
-
+    frame1.add(label1);
+    frame.setSize( 400, 400 );
+    frame1.setSize(600, 600);
     frame.setVisible( true );
-
+    frame1.setVisible(true);
     }
-
 }
